4179b411d46b24c77109656fdcadf37dff020c7d
NoahMaizels <noahmaizels@protonmail.com>
2023-12-11 16:46:42 +0700

    removed console.log statement

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = process.env.MONGO;
-console.log(uri)
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
